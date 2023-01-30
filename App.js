0380f0f5c7445f89742da0dd9da2bae277fa46ce
SOJEIN <JEISSON.OCHOA21@GMAIL.COM>
2023-01-30 10:06:52 -0500

    1.0.0
    Se agrega componentes de mapa y de modal

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Map, Modal } from './components';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Map />
+      <Modal />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
